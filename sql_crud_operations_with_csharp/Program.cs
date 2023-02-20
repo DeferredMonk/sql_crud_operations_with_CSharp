ad385b5284ed4efcd41793f5d2cc569803d46fad
DeferredMonk <marcoangeli151@gmail.com>
2023-02-20 13:58:11 +0200

    feat: added connection string and needed dependencies

--- sql_crud_operations_with_csharp/Program.cs
+++ sql_crud_operations_with_csharp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+static string GetConnectionString()
+{
+    var builder = new SqlConnectionStringBuilder
+    {
+        DataSource = "localhost\\SQLEXPRESS",
+        InitialCatalog = "Chinook",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true
+    };
+
+    return builder.ConnectionString;
+}
