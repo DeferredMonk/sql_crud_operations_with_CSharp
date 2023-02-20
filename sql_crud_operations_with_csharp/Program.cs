225386af57c1d9956ff9bd809c984446c9d9cf30
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-02-20 15:38:57 +0200

    Feat: find all customers

--- sql_crud_operations_with_csharp/Program.cs
+++ sql_crud_operations_with_csharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
+using sql_crud_operations_with_csharp.Repositories;
+using sql_crud_operations_with_csharp.Models;
+
 static string GetConnectionString()
 {
     var builder = new SqlConnectionStringBuilder
